d56de11172527a79e6bbb1c27906b3475d14b50b
Mohamed-alaa32 <mohammed.alaa200080@gmail.com>
2023-02-03 03:26:28 +0200

    add-functions-to-pure-pursuit

--- navigation/purepursuit.py
+++ navigation/purepursuit.py
@@ -102,3 +102,107 @@ class WayPoints:
 
         return ind, Lookahead # return the index of the target point and the look ahead distance
         
+def pure_pursuit_steer_control(state:State, trajectory:WayPoints, pind):
+    ind, Lf = trajectory.search_target_index(state)
+    tx = ty = 0
+    if pind >= ind:
+        ind = pind
+
+    if ind < len(trajectory.X_coordinates):
+        tx = trajectory.X_coordinates[ind]
+        ty = trajectory.Y_coordinates[ind]
+    else:  # toward goal
+        tx = trajectory.X_coordinates[-1]
+        ty = trajectory.Y_coordinates[-1]
+        ind = len(trajectory.X_coordinates) - 1
+
+    alpha = math.atan2(ty - state.rear_y, tx - state.rear_x) - state.yaw
+
+    delta = math.atan2(2.0 * BaseWidth * math.sin(alpha) / Lf, 1.0)
+
+    return delta, ind
+
+def proportional_control(targetSpeed, currentSpeed):  #longitudinal controller
+    
+    acc = Kp*(targetSpeed - currentSpeed) + Kd*((targetSpeed-currentSpeed)/dt )
+    return acc
+    
+def main():
+    
+    rospy.init_node ('purepursuit_controller', anonymous=True)
+    X_coordinates = np.arange(0, 100, 0.5)
+    Y_coordinates = [math.sin(ix / 5.0) * ix / 2.0 for ix in X_coordinates]
+    
+    target_speed = (20.0 / 3.6) #[m/s]
+    
+    T = 100.0  # max simulation time
+    
+    # initial state
+    state = State(x=-0.0, y=-3.0, yaw=0.0, currentSpeed=0.0)
+    
+    lastIndex = len(X_coordinates) - 1
+    time = 0.0
+    states = States()
+    states.update(time, state)
+    
+    target_course = WayPoints(X_coordinates, Y_coordinates) 
+    target_ind, _ = target_course.search_target_index(state)
+    
+    
+    while T >= time and lastIndex > target_ind:
+       
+        acc = proportional_control(target_speed, state.currentSpeed)  #longitudinal controller
+        di, target_ind = pure_pursuit_steer_control(state, target_course, target_ind) #lateral controller
+        state.update(acc, di)   #update the state of the car
+        
+        target_speed = (20.0/ 3.6)/(abs(di) *4)  # [m/s]
+        
+        if target_speed <= 10/3.6:  # min speed
+            target_speed = 10/3.6
+        if target_speed >= 40/3.6:   #max speed
+            target_speed = 40/3.6
+        time += dt
+        print("target_x:",round(target_course.X_coordinates[target_ind],2),"my_x:",round(state.x,2),
+         "target_y:" ,round(target_course.Y_coordinates[target_ind],2), "my_y:", round(state.y,2),"steer:",round(di,4),"speed:", round(state.currentSpeed,2),"ind:", target_ind, "target_Speed:", target_speed)
+        
+        states.update(time, state)
+        
+        if show_animation:  
+            plt.cla()
+            # for stopping simulation with the esc key.
+            plt.gcf().canvas.mpl_connect(
+                'key_release_event',
+                lambda event: [exit(0) if event.key == 'escape' else None])
+            
+            plt.plot(X_coordinates, Y_coordinates, "-r", label="course")
+            plt.plot(states.x, states.y, "-b", label="trajectory")
+            plt.plot(X_coordinates[target_ind], Y_coordinates[target_ind], "xg", label="target")
+            plt.axis("equal")
+            plt.grid(True)
+            plt.title("Speed[km/h]:" + str(state.currentSpeed * 3.6)[:4])
+            plt.pause(0.001)
+
+    assert lastIndex >= target_ind, "Cannot reach goal"
+    
+    if show_animation:  
+        plt.cla()
+        plt.plot(X_coordinates, Y_coordinates, ".r", label="course")
+        plt.plot(states.x, states.y, "-b", label="trajectory")
+        plt.legend()
+        plt.xlabel("x[m]")
+        plt.ylabel("y[m]")
+        plt.axis("equal")
+        plt.grid(True)
+
+        plt.subplots(1)
+        plt.plot(states.time, [iv * 3.6 for iv in states.currentSpeed], "-r")
+        plt.xlabel("Time[s]")
+        plt.ylabel("Speed[km/h]")
+        plt.grid(True)
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
+  
+        
