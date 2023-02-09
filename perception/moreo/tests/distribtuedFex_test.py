f756fadbc95c8c9c56fb2d8bc579f4d2fabc8b91
Ahmed Abdulmaksoud <ahmedsalahamaksoud@gmail.com>
2023-02-09 03:15:46 +0200

    refactor: seperated unit test from module

--- perception/moreo/tests/distribtuedFex_test.py
+++ perception/moreo/tests/distribtuedFex_test.py
@@ -1,156 +1,20 @@
-"""this module provides opencv like API for distributed
-feature extraction using a process pool
+"""test file for the distributed_fex module
 """
+# pylint: disable=all
+import sys
+
+import os
+
+CURRENT_DIR = os.getcwd()
+sys.path.append(os.path.join(CURRENT_DIR, "..", "src"))
+
 import time
-
-import multiprocessing
-from multiprocessing.managers import ListProxy
-
-from typing import List, Tuple
-
-import numpy as np
 
 import matplotlib.pyplot as plt
 
 import cv2 as cv
 
-
-def createMask(imageShape: Tuple[int], boundingBox: List[int]) -> np.ndarray:
-    """
-    creates a mask to mask the image and allow feature extraction
-    only to the selected areas
-
-    Parameters:
-    -----------
-    imageShape: Tuple[int]
-        shape of the input image
-    boundingBox: Tuple[int]
-        bounding in the form of [x1, y1, x2, y2]
-
-    Returns:
-        np.array
-            the mask of the region around the boundingBox
-    """
-    mask = np.zeros(imageShape, np.uint8)
-    mask[boundingBox[1] : boundingBox[3], boundingBox[0] : boundingBox[2]] = 255
-    return mask
-
-
-def createFeatureExtractionTask(
-    image: np.ndarray,
-    boundingBox: List[int],
-    keypointsList: List[float],
-    descriptorsList: List[float],
-) -> None:
-    """
-    task to extract the features of a single bounding box from the image,
-    used with threads to distribute the process of extracting the bounding boxes
-
-    Parameters:
-    ----------
-    image: np.array
-        input image
-    boundingBox: List[int]
-        bounding boxes in the form of [x1, y1, x2, y2]
-    keypointsList: List[float]
-        keypoints results array
-    descriptorsList: List[float]
-        descriptors results array
-    """
-    imageShape = image.shape
-
-    # creating mask step
-    mask = createMask(imageShape, boundingBox)
-    orb = cv.ORB_create(nfeatures=100)  # pylint: disable=c-extension-no-member
-
-    # orb feature extraction step
-    keypoint = orb.detect(image, mask)
-    keypoint, des = orb.compute(image, keypoint)
-    keypointsList.append(keypoint)
-    descriptorsList.append(des)
-
-
-class DistributedFeatureDetectionSystem:
-    """_summary_"""
-
-    def __init__(self) -> None:
-        """
-        Constructor of the distributed feature detector system.
-        It uses orb to detect features but in a distributed fashion
-
-        Parameters:
-        -----------
-        nFeatures: int Default=1000
-            max threshold of the orb features
-        """
-        self.pool = multiprocessing.Pool(processes=5)
-
-    def getFeatures(
-        self, image: np.ndarray, boundingBoxes: List[List[int]], visualize: bool = False
-    ) -> Tuple[ListProxy, ListProxy]:  # type: ignore
-        """
-        finds the features in the boundingBoxes of the image
-
-        Parameters:
-        -----------
-        image: np.array
-            input image
-        boundingBoxes: List[List[int]]
-            bounding boxes
-        visualize: bool Default=False
-            visualizes the feature extraction process using matplotlib
-
-        Returns:
-        --------
-        Tuple[ListProxy[Any], ListProxy[Any]]
-            tuple with keypoints at index=0 and descriptors at index=1
-
-        """
-        # results array
-        nboundingBoxes = len(boundingBoxes)
-        man = multiprocessing.Manager()
-        keypointsList = man.list()
-        descriptorsList = man.list()
-
-        # loop over all boundingBoxes
-
-        args = (
-            (image, boundingBoxes[i], keypointsList, descriptorsList) for i in range(nboundingBoxes)
-        )
-        res = self.pool.starmap_async(createFeatureExtractionTask, args)
-        res.wait()
-
-        if visualize:
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 10))
-            fig.suptitle("Moreo Results: boundingBoxes & Extracted Features")
-
-            visualizableImage = image.copy()
-            for i in range(nboundingBoxes):
-                boundingBox = boundingBoxes[i]
-                # bounding box
-                cv.rectangle(  # pylint: disable=c-extension-no-member
-                    visualizableImage,
-                    (boundingBox[0], boundingBox[1]),
-                    (boundingBox[2], boundingBox[3]),
-                    255,
-                    2,
-                )
-                # draw keypoints
-                keypoint = keypointsList[i]
-                visualizableImage = cv.drawKeypoints(  # pylint: disable=c-extension-no-member
-                    visualizableImage, keypoint, None, color=(0, 255, 0)
-                )
-
-            ax1.set_title("Before")
-            ax2.set_title("After")
-            ax1.imshow(image)
-            ax2.imshow(visualizableImage)
-            plt.show()
-
-        return keypointsList, descriptorsList
-
-
-####--- For Testing Purposes ---####
+from DistributedFex import DistributedFeatureDetectionSystem, createMask
 
 
 def getTimeDifferenceMS(snapshot1: int, snapshot2: int) -> float:
@@ -271,7 +135,8 @@ def miniTest() -> None:
         [378, 38, 443, 251],
     ]
 
-    image = plt.imread("121.jpg")
+    print(os.getcwd())
+    image = plt.imread(os.path.join(CURRENT_DIR, "121.jpg"))
 
     # feature extraction without distributed fex
     snapshot1 = time.time_ns()
