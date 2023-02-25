ad1afc9fe65c64a8ac7da4608d7397b308d7de72
ranaamr22 <106626014+ranaamr22@users.noreply.github.com>
2023-02-25 03:08:03 +0200

    test:adding test cases for smoreo

--- perception/smoreo/tests/smoreo_test.py
+++ perception/smoreo/tests/smoreo_test.py
@@ -23,30 +23,68 @@ class SmoreoTest(unittest.TestCase):
         frame="flir"
         smoreo = Smoreo({})
         self.assertEqual(smoreo.allLandMarks.header.frame_id, frame)    
-    def testTypeParam(self):
+    def testRaisingErrorAtInvalidParam(self):
         """
-        Test if the type of params is correct
+        Test if the error is raised when invalid params are passed
         """
-        smoreo = Smoreo({})
-        self.assertIsInstance(smoreo.params, dict)
+        with self.assertRaises(TypeError):
+            Smoreo("params")
     def testTypeLandmarkArray(self):
         """
         Test if the type of landmark array is correct
         """
         smoreo = Smoreo({})
-        self.assertIsInstance(smoreo.allLandMarks, LandmarkArray) 
-    def testTypeCutoffY(self):
+        self.assertIsInstance(smoreo.allLandMarks, LandmarkArray)
+    def testTypeErrorWithNoneParam(self):
+        """
+        Test if the error is raised when params are None
+        """
+        with self.assertRaises(TypeError):
+            Smoreo(None)
+    def testInvalidParam(self):
+        """
+        Test if the error is raised when invalid params are passed
+        """
+        with self.assertRaises(ValueError):
+            Smoreo({"cut_off_y":"cy"})
+        #self.assertEqual(str(context.exception),"Invalid Param") 
+    def testRaisingErrorAtInvalidBboxcy(self):
+        """
+        Test if the error is raised when invalid bboxcy is passed
+        """
+        smoreo = Smoreo({"cut_off_y":7.9})
+        with self.assertRaises(TypeError):
+            smoreo.filterNearBoxes("cy")
+    def testRaisingErrorAtInvalidCutoffY(self):
         """
         Test if the type of cut off y is correct
         """
-        smoreo = Smoreo({"cut_off_y":7.9})  
-        self.assertIsInstance(smoreo.params["cut_off_y"], float)
+        with self.assertRaises(TypeError):
+            Smoreo({"cut_off_y":"cy"})
     def testReturnTypeFilterNearBoxes(self):
         """
         Test if the return type of filterNearBoxes is correct
         """
         smoreo = Smoreo({"cut_off_y":7.9})
         self.assertIsInstance(smoreo.filterNearBoxes(0.5), bool)
+    def testOutputofInvalidBboxcy(self):
+        """
+        Test if the output of invalid bboxcy is correct
+        """
+        smoreo = Smoreo({"cut_off_y":50})
+        self.assertFalse(smoreo.filterNearBoxes(75))
+    def testOutputofValidBboxcy(self):
+        """
+        Test if the output of valid bboxcy is correct
+        """
+        smoreo = Smoreo({"cut_off_y":50})
+        self.assertTrue(smoreo.filterNearBoxes(25))
+    def testOutputofEqualBboxcy(self):
+        """
+        Test if the output of equal bboxcy to the cutoffy is valid
+        """
+        smoreo = Smoreo({"cut_off_y":50})
+        self.assertTrue(smoreo.filterNearBoxes(50))
 if __name__=='__main__':
     unittest.main()
     
