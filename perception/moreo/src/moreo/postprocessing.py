8ee1a83220f9e7f555b6d0f90d2b08e26deda275
ranaamr22 <106626014+ranaamr22@users.noreply.github.com>
2023-03-02 09:45:09 +0200

    style:some changes

--- perception/moreo/src/moreo/postprocessing.py
+++ perception/moreo/src/moreo/postprocessing.py
@@ -9,23 +9,23 @@ class Postprocessing:
     """
     This class is used to postprocess the output of the neural network.
     """
-    def postProcessing(self,myArray:npt.NDArray[np.float32],
-        mySize:npt.NDArray[np.float32],
-        topLeft:npt.NDArray[np.float32])-> npt.NDArray[np.float32]:
+    def post_processing(self,my_array:npt.NDArray[np.float32],
+        my_size:npt.NDArray[np.float32],
+        top_left:npt.NDArray[np.float32])-> npt.NDArray[np.float32]:
         """
         This function is used to postprocess the output of the neural network.
 
             Parameters:
-            myArray (numpy.ndarray): The output of the neural network.
-            mySize (numpy.ndarray): The size of the image.
-            topleft (numpy.ndarray): The top left corner of the image.
+            my_array (numpy.ndarray): The output of the neural network.
+            my_size (numpy.ndarray): The size of the image.
+            top_left (numpy.ndarray): The top left corner of the image.
 
             Returns:
             numpy.ndarray: The postprocessed output of the neural network.
         """
-        postArray=np.hstack((mySize,mySize,mySize,mySize,mySize,mySize,mySize,mySize))*myArray
-        postArray+=np.hstack((topLeft,topLeft,topLeft,topLeft,topLeft,topLeft,topLeft,topLeft))
-        return postArray
+        post_array=np.hstack((my_size,my_size,my_size,my_size,my_size,my_size,my_size,my_size))*my_array
+        post_array+=np.hstack((top_left,top_left,top_left,top_left,top_left,top_left,top_left,top_left))
+        return post_array
 my_array1=np.random.randint(0,54,(610,16))
 for_resizing=np.random.randint(0,54,(610,2))
 top_left1=np.random.randint(0,54,(610,2))
@@ -35,7 +35,7 @@ ITERS = 100000
 for i in range(ITERS):
     a = datetime.datetime.now()
     postprocessor = Postprocessing() 
-    postprocessed_array = postprocessor.postProcessing(my_array1, for_resizing, top_left1) 
+    postprocessed_array = postprocessor.post_processing(my_array1, for_resizing, top_left1) 
     b = datetime.datetime.now()
     SUM+=(b - a).microseconds*1e-3
 
