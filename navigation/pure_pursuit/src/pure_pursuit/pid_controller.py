e789aa2aaab70a8d97153c4bfc5e256dccdf9412
Mohamed-alaa32 <mohammed.alaa200080@gmail.com>
2023-03-21 05:11:54 +0200

    docs: add docstrings to pid

--- navigation/pure_pursuit/src/pure_pursuit/pid_controller.py
+++ navigation/pure_pursuit/src/pure_pursuit/pid_controller.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
+
 """
-PID Controller
-"""
-# import rospy
+PID controller module used to control the throttle and velocity of the vehicle
 
 
-KP = 0.1  # rospy.get_param("/gains/proportional")  # propotional gain
-KD = 0.1  # rospy.get_param("/gains/differential")  # differential gain
-KI = 0.1  # rospy.get_param("/gains/integral")  # integral gain
+parameters
+----------
+KP : float
+    proportional gain for the PID controller
+KD : float
+    differential gain for the PID controller
+KI : float
+    integral gain for the PID controller
+DT : float
+    time step for the PID controller
+MAXSPEED : float
+    maximum speed allowed to be set for the vehicle
+MINSPEED : float
+    minimum speed allowed to be set for the vehicle
+TARGETSPEED : float
+    constant target speed for the vehicle
+MAXACC : float
+    maximum acceleration allowed to be set for the vehicle
+MINACC : float
+    minimum acceleration allowed to be set for the vehicle
+"""
+KP = 0.5  # rospy.get_param("/gains/proportional")  # propotional gain
+KD = 0.4  # rospy.get_param("/gains/differential")  # differential gain
+KI = 0.5  # rospy.get_param("/gains/integral")  # integral gain
 DT = 0.1  # rospy.get_param("/time_step")  # [s] time step
-MAXSPEED = 15  # rospy.get_param("/max_speed")  # [m/s] max speed
+MAXSPEED = 20  # rospy.get_param("/max_speed")  # [m/s] max speed
 MINSPEED = 2  # rospy.get_param("/min_speed")  # [m/s] min speed
 TARGETSPEED = 10  # rospy.get_param("/target_speed")  # [m/s] target speed
 MAXACC = 15  # rospy.get_param("/max_acceleration")  # [m/ss] max acceleration
@@ -18,9 +38,19 @@ MINACC = -15  # rospy.get_param("/min_acceleration")  # [m/ss] min acceleration
 
 def targetSpeedCalc(delta: float) -> float:
     """
-    Calculating Target Speed for PID controller , velocity profile
+    calculates the target speed based on the curvature of the path
+
+    Parameters
+    ----------
+    delta : float
+        steering angle of the vehicle
+
+    Returns
+    -------
+    targetSpeed : float
+        calculated target speed of the vehicle
     """
-    targetSpeed: float = (TARGETSPEED) / (abs(delta) * 4)  # [m/s]
+    targetSpeed: float = TARGETSPEED / (abs(delta) * 4 + 0.0001)  # [m/s]
     # waypoints.update(pose)
     targetSpeed = max(targetSpeed, MINSPEED)
     targetSpeed = min(targetSpeed, MAXSPEED)
