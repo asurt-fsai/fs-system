1b07f5d8641f4b6cc2a66dd76d6cf62c0cc7a784
Mohamed-alaa32 <mohammed.alaa200080@gmail.com>
2023-04-25 10:23:15 +0200

    fix: fix minor issues

--- navigation/pure_pursuit/nodes/purepursuit_node.py
+++ navigation/pure_pursuit/nodes/purepursuit_node.py
@@ -1,12 +1,20 @@
 """
 Initilization Pure Pursuit node for vehicle control
 """
-import math
+# import math
 import rospy
 from ackermann_msgs.msg import AckermannDrive
 from geometry_msgs.msg import Pose
-from pure_pursuit import WayPoints, State, purepursuitSteercontrol, proportionalControl, Position
-import matplotlib.pyplot as plt
+from pure_pursuit import (
+    WayPoints,
+    State,
+    PidController,
+    purepursuitSteercontrol,
+    Position,
+    plot,
+)
+
+from nav_msgs.msg import Path
 
 
 PLOTTING = rospy.get_param("/plotting")
@@ -29,10 +37,10 @@ def main() -> None:
     position = Position(0.0, 0.0)
     state = State(position, 0.0)
     rospy.Subscriber("/state", Pose, callback=state.update)
-    rospy.Subscriber("/waypoints", Pose, callback=waypoints.add)
+    rospy.Subscriber("/waypoints", Path, callback=waypoints.add)
 
     controlAction = AckermannDrive()
-
+    pidController = PidController()
     rate = rospy.Rate(10)
     targetInd = 0
     prevError = 0
@@ -40,14 +48,14 @@ def main() -> None:
 
         delta, targetInd = purepursuitSteercontrol(state, waypoints, targetInd)
 
-        acc, error = proportionalControl(TARGETSPEED, state.currentSpeed, prevError)
+        acc, error = pidController.proportionalControl(TARGETSPEED, state.currentSpeed, prevError)
         # longitudinal controller
         prevError = error
-        deltaDegree = math.degrees(delta)
+        # deltaDegree = math.degrees(delta)
         controlAction.acceleration = acc  # proportionalControl(targetSpeed, state.currentSpeed)
         controlAction.steering_angle = delta
         controlAction.jerk = targetInd
-        controlAction.speed = deltaDegree
+        controlAction.speed = TARGETSPEED
         controlAction.steering_angle_velocity = waypoints.yList[targetInd]
 
         controlActionPub.publish(controlAction)
@@ -56,30 +64,6 @@ def main() -> None:
             plot(waypoints, state, PLOTNAME, targetInd)
 
         rate.sleep()
-
-    # assert lastIndex >= targetInd, "Cannot reach goal"
-
-
-def plot(waypoints: WayPoints, state: State, name: str, targetInd: int) -> None:
-    """
-    plot the waypoints and the current state of the vehicle
-
-    Args:
-        waypoints (WayPoints): waypoints to follow
-
-        state (State): current state of the vehicle
-
-        name (str): name of the plot
-    """
-    plt.plot(waypoints.xList, waypoints.yList, "-r", label="course")
-    # plt.plot(waypoints.xList[targetInd], waypoints.yList[targetInd], "xg", label="target")
-    plt.plot(state.position.x, state.position.y, "ob", label="state")
-    plt.grid(True)
-    plt.plot(waypoints.xList[targetInd], waypoints.yList[targetInd], "xg", label="target")
-    plt.axis("equal")
-    plt.title(name)
-    # plt.legend()
-    plt.pause(0.001)
 
 
 if __name__ == "__main__":
