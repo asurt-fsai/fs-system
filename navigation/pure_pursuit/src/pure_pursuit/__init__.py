a563bc1b6b9c0719ca6b0937ca1157ea7e9fcb92
Mohamed-alaa32 <mohammed.alaa200080@gmail.com>
2023-04-25 10:24:35 +0200

    test: add new test file

--- navigation/pure_pursuit/src/pure_pursuit/__init__.py
+++ navigation/pure_pursuit/src/pure_pursuit/__init__.py
@@ -1,4 +1,5 @@
 # pylint: disable=all
 # mypy: ignore-errors
-from .pure_pursuit import purepursuitSteercontrol, State, WayPoints, States, Position
-from .pid_controller import throttleControl, proportionalControl
+from .pure_pursuit import *
+from .pid_controller import *
+from .visualizer import *
---
+++ navigation/pure_pursuit/testing/pathMsgTesting.py
@@ -0,0 +1,33 @@
+# pylint: disable=all
+# mypy: ignore-errors
+import rospy
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+import matplotlib.pyplot as plt
+
+rospy.init_node("pathMsgTesting", anonymous=True)
+pathPub = rospy.Publisher("/path", Path, queue_size=10)
+path = Path()
+path.header.frame_id = "map"
+path.header.stamp = rospy.Time.now()
+
+pose = PoseStamped()
+pose.pose.position.x = 1.0
+pose.pose.position.y = 2.0
+pose.pose.orientation.z = 3.0
+pose2 = PoseStamped()
+pose2.pose.position.x = 4.0
+pose2.pose.position.y = 5.0
+pose2.pose.orientation.z = 6.0
+path.poses.append(pose)
+path.poses.append(pose2)
+
+if __name__ == "__main__":
+    try:
+        while not rospy.is_shutdown():
+            pathPub.publish(path)
+            rospy.sleep(1)
+            print(type(path.poses[0]))
+
+    except rospy.ROSInterruptException:
+        pass
