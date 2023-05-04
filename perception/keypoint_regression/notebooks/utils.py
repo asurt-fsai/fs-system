223e373efa0a44588ffa84d01008306669b211e3
Ahmed Abdulmaksoud <ahmedsalahamaksoud@gmail.com>
2023-05-04 18:40:45 +0300

    feat: add modelname function

--- perception/keypoint_regression/notebooks/utils.py
+++ perception/keypoint_regression/notebooks/utils.py
@@ -1,11 +1,16 @@
 """Utils used for keypoint regression."""
+# mypy: ignore-errors
+
+from datetime import datetime
 
 import torch
+
+import numpy as np
 
 import matplotlib.pyplot as plt
 
 
-def showImageTensor(img: torch.Tensor) -> None:
+def showImageTensor(img: torch.Tensor, show=True) -> None:
     """
     shows input image
 
@@ -14,6 +19,23 @@ def showImageTensor(img: torch.Tensor) -> None:
         img: torch.tensor
             image to show
     """
-    img = img.detach().squeeze(0).permute((1, 2, 0)).cpu().numpy()
+    img = img.cpu().detach().permute(1, 2, 0).squeeze(2).numpy().astype(np.float32).copy()
     plt.imshow(img)
-    plt.show()
+    if show:
+        plt.show()
+
+
+def getFormattedModelName(name: str) -> str:
+    """
+    gets the datetime in string format
+
+    Parameters:
+    -----------
+        name: str
+            name of the model
+    Returns:
+    --------
+        str:
+            formatted datetime in string format
+    """
+    return name + "_" + str(datetime.utcnow()).split(".", maxsplit=1)[0].replace(":", "-") + ".pth"
