9fb749d3bb9419a8b710467767b79526bd2a8e6a
Ahmed Abdulmaksoud <ahmedsalaheldin125@gmail.com>
2023-06-26 16:09:36 +0300

    feat: add models

--- perception/keypoint_regression/__init__.py
+++ perception/keypoint_regression/__init__.py
@@ -1,80 +1,80 @@
-# mypy: ignore-errors
-"""Fascade for keypoint regression."""
+# # mypy: ignore-errors
+# """Fascade for keypoint regression."""
 
-from typing import List
+# from typing import List
 
-import numpy.typing as npt
+# import numpy.typing as npt
 
-import torch
+# import torch
 
-from model import KeypointNet
+# from model import KeypointNet
 
-from preprocessing import ImagePreprocessing
+# from preprocessing import ImagePreprocessing
 
-from postprocessing import PostProcessing
+# from postprocessing import PostProcessing
 
 
-class KeypointsRegressionFascade:  # pylint: disable = too-few-public-methods
+# class KeypointsRegressionFascade:  # pylint: disable = too-few-public-methods
 
-    """Fascade for keypoint regression."""
+#     """Fascade for keypoint regression."""
 
-    IMAGE_WIDTH = 80  # pylint: disable=invalid-name
-    IMAGE_HEIGHT = 80  # pylint: disable=invalid-name
+#     IMAGE_WIDTH = 80  # pylint: disable=invalid-name
+#     IMAGE_HEIGHT = 80  # pylint: disable=invalid-name
 
-    def __init__(self, modelPath: str):
-        """
-        Follows the Fascade design pattern to provide a single interface to the
-        keypoint regression module.
+#     def __init__(self, modelPath: str):
+#         """
+#         Follows the Fascade design pattern to provide a single interface to the
+#         keypoint regression module.
 
-        Parameters:
-        -----------
-            modelPath: str
-                path of the model to be used for keypoint regression
-        """
-        # model
-        self.modelPath = modelPath
-        self.model = KeypointNet(inputChannels=1, outKeypoints=8)
-        self.model.load_state_dict(torch.load(self.modelPath))
-        self.model.eval()
+#         Parameters:
+#         -----------
+#             modelPath: str
+#                 path of the model to be used for keypoint regression
+#         """
+#         # model
+#         self.modelPath = modelPath
+#         self.model = KeypointNet(inputChannels=1, outKeypoints=8)
+#         self.model.load_state_dict(torch.load(self.modelPath))
+#         self.model.eval()
 
-        # preprocessing
-        self.imagePreprocessing = ImagePreprocessing()
+#         # preprocessing
+#         self.imagePreprocessing = ImagePreprocessing()
 
-        # postprocessing
-        self.postProcessing = PostProcessing()
+#         # postprocessing
+#         self.postProcessing = PostProcessing()
 
-    def run(self, img: npt.NDArray, bboxes: List[List[int]]):
-        """
-        Runs the keypoint regression module on the given image and bounding boxes
+#     def run(self, img: npt.NDArray, bboxes: List[List[int]]):
+#         """
+#         Runs the keypoint regression module on the given image and bounding boxes
 
-        Parameters:
-        -----------
-            img: np.ndarray
-                image to run the keypoint regression on
-            bboxes: List[List[int]]
-                bounding boxes to run the keypoint regression on
+#         Parameters:
+#         -----------
+#             img: np.ndarray
+#                 image to run the keypoint regression on
+#             bboxes: List[List[int]]
+#                 bounding boxes to run the keypoint regression on
 
-        Returns:
-        --------
-            keypoints: List[List[int]]
-                list of keypoints
-        """
-        # preprocessing
-        (
-            bboxesImages,
-            boundingBoxTransformationMatrix,
-            originalBboxesDimsMatrix,
-        ) = self.imagePreprocessing.runPreprocessingPipeline(
-            img, self.IMAGE_WIDTH, self.IMAGE_HEIGHT, bboxes
-        )
+#         Returns:
+#         --------
+#             keypoints: List[List[int]]
+#                 list of keypoints
+#         """
+#         # preprocessing
+#         (
+#             bboxesImages,
+#             boundingBoxTransformationMatrix,
+#             originalBboxesDimsMatrix,
+#         ) = self.imagePreprocessing.runPreprocessingPipeline(
+#             img, self.IMAGE_WIDTH, self.IMAGE_HEIGHT, bboxes
+#         )
 
-        # model
-        keypoints = self.model(bboxesImages.cuda())
-        keypoints = keypoints.detach().cpu().numpy()
+#         # model
+#         keypoints = self.model(bboxesImages.cuda())
+#         keypoints = keypoints.detach().cpu().numpy()
 
-        # postprocessing
-        keypoints = self.postProcessing(
-            keypoints, originalBboxesDimsMatrix, boundingBoxTransformationMatrix
-        )
+#         # postprocessing
+#         keypoints = self.postProcessing(
+#             keypoints, originalBboxesDimsMatrix, boundingBoxTransformationMatrix
+#         )
 
-        return keypoints
+#         return keypoints
