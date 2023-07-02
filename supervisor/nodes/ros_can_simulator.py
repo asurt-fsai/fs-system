f66e96dd9656dc0e1d3ced02e0d90dc52d081dca
Heba2h <99084258+Heba2h@users.noreply.github.com>
2023-07-02 16:50:46 +0300

    feat: add roscan simulator

---
+++ supervisor/nodes/ros_can_simulator.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+.
+
+"""
+import rospy
+from std_msgs.msg import Float32, Bool
+from ackermann_msgs.msg import AckermannDriveStamped
+from supervisor.helpers.rosCanSimulator import RosCanSimulator
+
+
+def main() -> None:
+    """
+    Main function.
+    """
+
+    rospy.init_node("ros_can_simulator")
+    drivingFlagTopic = rospy.get_param("/supervisor/driving_flag")
+    missionFLagTopic = rospy.get_param("/supervisor/mission_flag")
+    rosCanCmdTopic = rospy.get_param("/ros_can/cmd")
+    vcuCurrVelTopic = rospy.get_param("/vcu/curr_vel")
+    vcuVelTopic = rospy.get_param("/vcu/control_vel")
+    vcuSteerTopic = rospy.get_param("/vcu/control_steer")
+    rosCanStateTopic = rospy.get_param("/ros_can/can_state")
+    rosCanVelTopic = rospy.get_param("/ros_can/twist")
+
+    rosCanSimulator = RosCanSimulator(
+        11, vcuVelTopic, vcuSteerTopic, rosCanStateTopic, rosCanVelTopic
+    )
+    rospy.Subscriber(drivingFlagTopic, Bool, rosCanSimulator.drivingFlagCallback)
+    rospy.Subscriber(missionFLagTopic, Bool, rosCanSimulator.missionFlagCallback)
+    rospy.Subscriber(rosCanCmdTopic, AckermannDriveStamped, rosCanSimulator.cmdCallback)
+    rospy.Subscriber(vcuCurrVelTopic, Float32, rosCanSimulator.currentVelCallback)
+
+    rate = rospy.Rate(100)
+    while not rospy.is_shutdown():
+        rosCanSimulator.run()
+        rate.sleep()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
---
+++ supervisor/src/supervisor/helpers/rosCanSimulator.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python3
+"""
+.
+
+"""
+
+from typing import Any
+
+from enum import Enum
+
+import rospy
+from std_msgs.msg import Float32
+
+from std_msgs.msg import Bool
+from eufs_msgs.msg import CanState
+from ackermann_msgs.msg import AckermannDriveStamped
+from geometry_msgs.msg import TwistWithCovarianceStamped
+from std_srvs.srv import Trigger, TriggerResponse
+
+
+class VCUState(Enum):
+    """
+    VCU state enumeration.
+    """
+
+    AS_OFF = 0
+    AS_READY = 1
+    AS_DRIVING = 2
+    AS_FINISHED = 3
+    AS_EBS = 4
+
+
+class RosCanSimulator:  # pylint: disable=too-many-instance-attributes
+    """
+    ROS CAN simulator class.
+    """
+
+    def __init__(
+        self,
+        missionSelected: int,
+        vcuVelTopic: str,
+        vcuSteerTopic: str,
+        rosCanStateTopic: str,
+        rosCanVelTopic: str,
+    ) -> None:
+        self.missionSelected = missionSelected
+        self.drivingFlag = False
+        self.missionFlag = False
+        self.cmdVel = 0.0
+        self.cmdSteer = 0.0
+        self.currentVel = 0.0
+
+        self.canStatePub = rospy.Publisher(rosCanStateTopic, CanState, queue_size=10)
+        self.currentVelPub = rospy.Publisher(
+            rosCanVelTopic, TwistWithCovarianceStamped, queue_size=10
+        )
+        self.velPub = rospy.Publisher(vcuVelTopic, Float32, queue_size=10)
+        self.steerPub = rospy.Publisher(vcuSteerTopic, Float32, queue_size=10)
+
+        self.curState = VCUState.AS_OFF
+        rospy.Service("vcu_next_state", Trigger, self.nextState)
+        rospy.Service("vcu_prev_state", Trigger, self.prevState)
+        rospy.Service("vcu_EBS", Trigger, self.ebs)
+
+    def nextState(self, _: Any) -> str:
+        """
+        Next state
+        """
+        self.curState = VCUState(min(self.curState.value + 1, 4))
+        response = TriggerResponse()
+        response.success = True
+        response.message = "Success"
+        return response  # type: ignore
+
+    def prevState(self, _: Any) -> str:
+        """
+        Previous state
+        """
+        self.curState = VCUState(max(self.curState.value - 1, 0))
+        response = TriggerResponse()
+        response.success = True
+        response.message = "Success"
+        return response  # type: ignore
+
+    def ebs(self, _: Any) -> str:
+        """
+        Emergency brake service
+        """
+        self.curState = VCUState.AS_EBS
+        response = TriggerResponse()
+        response.success = True
+        response.message = "Success"
+        return response  # type: ignore
+
+    def drivingFlagCallback(self, msg: Bool) -> None:
+        """
+        Callback for the driving flag
+        """
+        self.drivingFlag = msg.data
+
+    def missionFlagCallback(self, msg: Bool) -> None:
+        """
+        Callback for the mission flag
+        """
+        self.missionFlag = msg.data
+
+    def cmdCallback(self, msg: AckermannDriveStamped) -> None:
+        """
+        Callback for the drive command
+        """
+        if self.curState == VCUState.AS_DRIVING:
+            self.cmdVel = msg.drive.speed
+            self.cmdSteer = msg.drive.steering_angle
+        else:
+            self.cmdVel = 0
+            self.cmdSteer = 0
+
+    def currentVelCallback(self, msg: Float32) -> None:
+        """
+        Callback for the current velocity
+        """
+        self.currentVel = msg.data
+
+    def run(self) -> None:
+        """
+        Publish velocity and state
+        """
+        velMsg = TwistWithCovarianceStamped()
+        velMsg.twist.twist.linear.x = self.currentVel
+        velMsg.header.stamp = rospy.Time.now()
+        self.currentVelPub.publish(velMsg)
+
+        canState = CanState()
+        canState.as_state = self.curState.value
+        canState.ami_state = self.missionSelected
+        self.canStatePub.publish(canState)
+
+        self.velPub.publish(self.cmdVel)
+        self.steerPub.publish(self.cmdSteer)
