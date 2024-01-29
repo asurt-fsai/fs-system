5703eb4bd17866e8bccae328449a2a49eb02f599
Mohamed Alaa <mohammed.alaa200080@gmail.com>
2024-01-29 21:34:43 +0200

    feat: switch ros1 to ros2

--- navigation/simple_pure_pursuit/setup.py
+++ navigation/simple_pure_pursuit/setup.py
@@ -1,9 +1,27 @@
-"""
-Install simple pure pursuit library
-"""
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import find_packages, setup
 
-d = generate_distutils_setup(packages=["simple_pure_pursuit"], package_dir={"": "src"})
+package_name = 'simple_pure_pursuit'
 
-setup(**d)
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mohamedalaa',
+    maintainer_email='mohammed.alaa200080@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'simple_pp_node = simple_pure_pursuit.simple_pp_node:main',
+            'waypoints_node = simple_pure_pursuit.waypoints_node:main',
+        ],
+    },
+)
