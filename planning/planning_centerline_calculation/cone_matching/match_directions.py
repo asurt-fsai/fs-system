161390ad087552b4f8d9cd5a9c7124fc897b3d41
farah-salama <farahhsalamaa@gmail.com>
2024-01-30 17:42:45 +0200

    Update match_directions.py

--- planning/planning_centerline_calculation/cone_matching/match_directions.py
+++ planning/planning_centerline_calculation/cone_matching/match_directions.py
@@ -1,44 +1,44 @@
 import numpy as np
 
-'''from planning_centerline_calculation.utils.cone_types import ConeTypes
+from planning_centerline_calculation.utils.cone_types import ConeTypes
 from planning_centerline_calculation.utils.math_utils import my_njit, rotate
 
 
-@my_njit'''
-def calculate_search_direction_for_one(cones, idxs, cone_type):
+@my_njit
+def calculateSearchDirectionForOne(cones, idxs, cone_type):
     """
     Calculates the search direction for one cone
     """
     assert len(idxs) == 2
 
-    track_direction = cones[idxs[1]] - cones[idxs[0]]
+    trackDirection = cones[idxs[1]] - cones[idxs[0]]
 
-    rotation_angle = np.pi / 2 if cone_type == ConeTypes.RIGHT else -np.pi / 2
+    rotationAngle = np.pi / 2 if cone_type == ConeTypes.RIGHT else -np.pi / 2
 
-    search_direction = rotate(track_direction, rotation_angle)
+    searchDirection = rotate(trackDirection, rotationAngle)
 
-    return search_direction / np.linalg.norm(search_direction)
+    return searchDirection / np.linalg.norm(searchDirection)
 
 
 #@my_njit
-def calculate_match_search_direction(
+def calculateMatchSearchDirection(
     cones,
-    cone_type: ConeTypes,
+    coneType: ConeTypes,
 ):
-    number_of_cones = len(cones)
-    assert number_of_cones > 1
+    numberOfCones = len(cones)
+    assert numberOfCones > 1
 
-    cones_xy = cones[:, :2]
+    conesXY = cones[:, :2]
 
-    out = np.zeros((number_of_cones, 2))
-    out[0] = calculate_search_direction_for_one(cones_xy, np.array([0, 1]), cone_type)
-    out[-1] = calculate_search_direction_for_one(
-        cones_xy, np.array([-2, -1]), cone_type
+    out = np.zeros((numberOfCones, 2))
+    out[0] = calculateSearchDirectionForOne(conesXY, np.array([0, 1]), coneType)
+    out[-1] = calculateSearchDirectionForOne(
+        conesXY, np.array([-2, -1]), coneType
     )
 
-    for i in range(1, number_of_cones - 1):
-        out[i] = calculate_search_direction_for_one(
-            cones_xy, np.array([i - 1, i + 1]), cone_type
+    for i in range(1, numberOfCones - 1):
+        out[i] = calculateSearchDirectionForOne(
+            conesXY, np.array([i - 1, i + 1]), coneType
         )
 
     return out
