d62d20a87d56a71ad80397198fc0203ce18e812c
Mazen-Elaraby <mazenelaraby9@gmail.com>
2024-02-26 02:32:58 +0200

    feat: add camera_interface launch file. refactor: refactored camera_interface_node

--- perception/camera_interface/camera_interface/camera_interface_node.py
+++ perception/camera_interface/camera_interface/camera_interface_node.py
@@ -16,45 +16,117 @@ class CameraNode(Node):
         super().__init__("camera_interface_node")
         self.get_logger().info("STARTING CAMERA NODE...")
 
-        #node parameters: should be moved to launch file and fetched here
-        #video feed params
-        self.RATE = 30 # Hz/fps
-        self.WIDTH = 640
-        self.HEIGHT = 480
-        #camera intrinsics
-        cx = 0
-        cy = 0
-        fx = 0
-        fy = 0
-        #distortion coeff.
-        k1 = 0
-        k2 = 0
-        p1 = 0
-        p2 = 0
-        k3 = 0
+        #declaring params
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('/camera_interface/rate', rclpy.Parameter.Type.INTEGER),
+                ('/camera_interface/image_width', rclpy.Parameter.Type.INTEGER),
+                ('/camera_interface/image_height', rclpy.Parameter.Type.INTEGER),
+                ('/camera_interface/cx', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/cy', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/fx', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/fy', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/k1', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/k2', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/p1', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/p2', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/k3', rclpy.Parameter.Type.DOUBLE),
+                ('/camera_interface/camera_feed', rclpy.Parameter.Type.STRING)
+            ]
+        )
 
-        self.camera_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]]) 
-        self.dist_coeffs = np.array([[k1], [k2], [p1], [p2], [k3]])
-
-        # Generate undistortion maps
-        self.mapx, self.mapy = cv2.initUndistortRectifyMap(self.camera_matrix, self.dist_coeffs, None, None, (self.WIDTH, self.HEIGHT), cv2.CV_32FC1)
-
-        self.timer = self.create_timer(1/self.RATE, self.callback_camera)
-        self.img_pub = self.create_publisher(Image, "/camera_interface/camera_feed", 10)
+        self.timer = None
+        self.img_publisher = None
 
         self.bridge = CvBridge()
-        self.cap = cv2.VideoCapture(0)
+        self.cap = None
         self.frame_id = 0
+        
+        #undistortion maps
+        self.mapx = None
+        self.mapy = None 
 
-        # Set camera resolution
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.WIDTH)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.HEIGHT)
-        # Set the capture FPS 
-        self.cap.set(cv2.CAP_PROP_FPS, self.RATE)
+        self.start()
+    
+    def get_params(self):
+        try:
+            # Video feed params
+            rate = self.get_parameter('/camera_interface/rate').get_parameter_value().integer_value  # Hz/fps
+            width = self.get_parameter('/camera_interface/image_width').get_parameter_value().integer_value
+            height = self.get_parameter('/camera_interface/image_height').get_parameter_value().integer_value
+            # Camera intrinsics
+            cx = self.get_parameter('/camera_interface/cx').get_parameter_value().double_value
+            cy = self.get_parameter('/camera_interface/cy').get_parameter_value().double_value
+            fx = self.get_parameter('/camera_interface/fx').get_parameter_value().double_value
+            fy = self.get_parameter('/camera_interface/fy').get_parameter_value().double_value
+            # Distortion coefficients
+            k1 = self.get_parameter('/camera_interface/k1').get_parameter_value().double_value
+            k2 = self.get_parameter('/camera_interface/k2').get_parameter_value().double_value
+            p1 = self.get_parameter('/camera_interface/p1').get_parameter_value().double_value
+            p2 = self.get_parameter('/camera_interface/p2').get_parameter_value().double_value
+            k3 = self.get_parameter('/camera_interface/k3').get_parameter_value().double_value
+
+            # Constructing and returning the params dictionary
+            params = {
+                'rate': rate,
+                'width': width,
+                'height': height,
+                'cx': cx,
+                'cy': cy,
+                'fx': fx,
+                'fy': fy,
+                'k1': k1,
+                'k2': k2,
+                'p1': p1,
+                'p2': p2,
+                'k3': k3
+            }
+
+            # Printing fetched parameters for confirmation
+            self.get_logger().info(f"Fetched parameters successfully: {params}")
+            return params
+
+        except Exception as e:
+            # Logging the error and re-raising
+            self.get_logger().error(f"Failed to fetch parameters: {e}")
+
+    def generate_undistort_maps(self, params):
+
+        camera_matrix = np.array([[params['fx'], 0, params['cx']], [0, params['fy'], params['cy']], [0, 0, 1]]) 
+        dist_coeffs = np.array([[params['k1']], [params['k2']], [params['p1']], [params['p2']], [params['k3']]])
+
+        # Generate undistortion maps
+        self.mapx, self.mapy = cv2.initUndistortRectifyMap(camera_matrix, dist_coeffs, None, None, (params['width'], params['height']), cv2.CV_32FC1)
+        
+
+    def start(self):
+
+        #fetch parameters from launch file
+        params = self.get_params()
+
+        #define timer and publisher
+        self.timer = self.create_timer(1/params['rate'], self.callback_camera)
+        camera_feed_topic = self.get_parameter('/camera_interface/camera_feed').get_parameter_value().string_value
+        self.img_publisher = self.create_publisher(Image, camera_feed_topic, 10)
+
+        #create undistortion maps
+        self.generate_undistort_maps(params)
+
+        #set-up camera capture 
+        self.cap = cv2.VideoCapture(0)
 
         if not self.cap.isOpened():
             self.get_logger().error('Could not open camera')
             sys.exit(1)
+
+        #set camera capture resolution
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, params['width'])
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, params['height'])
+        
+        #set camera capture FPS
+        self.cap.set(cv2.CAP_PROP_FPS, params['rate'])
+        
 
     def callback_camera(self):
 
@@ -75,7 +147,7 @@ class CameraNode(Node):
             ros_image.header.frame_id = str(self.frame_id)
             
             # Publish the image
-            self.img_pub.publish(ros_image)
+            self.img_publisher.publish(ros_image)
 
             self.get_logger().info('Publishing webcam image' + str(self.frame_id))
             self.frame_id += 1
--- perception/camera_interface/setup.py
+++ perception/camera_interface/setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import find_packages, setup
 
 package_name = 'camera_interface'
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), ['launch/camera_interface_launch.launch.xml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
