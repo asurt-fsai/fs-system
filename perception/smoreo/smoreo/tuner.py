0ecb7141cdef85a94ac55c1a009534838c9a8122
faridaelhusseiny278 <faridaelhussieny@gmail.com>
2024-03-16 03:35:53 +0200

    add tuner

--- perception/smoreo/smoreo/tuner.py
+++ perception/smoreo/smoreo/tuner.py
@@ -4,44 +4,53 @@ Main ros node for the smoreo tuner
 #!/usr/bin/python3
 import sys
 # import rospy
-from tf_helper.StatusPublisher import StatusPublisher
-from smoreo.tuner.tunerServer import Tuner
+# from tf_helper.StatusPublisher import StatusPublisher
+# from smoreo.tuner.tunerServer import Tuner
 import rclpy
 from rclpy.node import Node
 import os
 from typing import Any, Union
 import yaml
-from dynamic_reconfigure.server import Server
+# from dynamic_reconfigure.server import Server
 from sensor_msgs.msg import Image
 import cv2 as cv
 from cv_bridge import CvBridge
 import numpy as np
 from numpy.typing import NDArray
-from smoreo.cfg import smoreoConfig  # type: ignore
+# from smoreo.cfg import smoreoConfig  # type: ignore
 
 class TunerSystem(Node):
     def __init__(self):
         super().__init__("tuner")
 
-        self.srv: Server
-        self.cutOffPublisher: rclpy.publisher
+        # self.srv: Server
+        self.cutOffPublisher: rclpy.publisher.Publisher
         self.bridge: CvBridge
         self.lastImage: Union[NDArray[np.float32], None]
 
-
+        # self.declare_parameters(
+        #     namespace=" ",
+        #     parameters=[
+        #        ("smoreo/f", rclpy.Parameter.Type.DOUBLE),
+        #         ("smoreo/cx", rclpy.Parameter.Type.DOUBLE),
+        #         ("smoreo/cy", rclpy.Parameter.Type.DOUBLE),
+        #         ("smoreo/cone_height", rclpy.Parameter.Type.DOUBLE),
+        #         ("smoreo/camera_height_from_ground", rclpy.Parameter.Type.DOUBLE),
+        #         ("smoreo/cut_off_y", rclpy.Parameter.Type.INTEGER),
+               
+        #     ],
+        # )
 
         self.create_timer(0.1, self.timer_callback)
-        self.tunerStatus = StatusPublisher("/status/tuner")
-        self.tunerStatus.starting()
+        # self.tunerStatus = StatusPublisher("/status/tuner")
+        # self.tunerStatus.starting()
         # self.tuner = Tuner(self)
         self.start()
-        self.tunerStatus.ready()
+        # self.tunerStatus.ready()
        
-    def timer_callback(self):        
-        self.visualizeCutOff()
-        self.tunerStatus.running()
+    
 
-    def parametersCallback(self, config: smoreoConfig, level: Any) -> smoreoConfig:
+    def parametersCallback(self, config, level: Any):
         """
         Callback for dynamic reconfigure server
         Parameters:
@@ -62,7 +71,7 @@ class TunerSystem(Node):
         Visualize the cut off line
         """
         if self.lastImage is not None:
-            cutOffy = self.get_parameter("smoreo/cut_off_y")
+            cutOffy = self.get_parameter("smoreo/cut_off_y").get_parameter_value().integer_value
             startPoint = (0, cutOffy)
             endPoint = (self.lastImage.shape[1], cutOffy)
             out = cv.line(self.lastImage.copy(), startPoint, endPoint, (0, 0, 0), 10)
@@ -90,9 +99,9 @@ class TunerSystem(Node):
                              -/smoreo/camera_raw\n \
                              -/smoreo/cut_off_viz."
             )
-        self.srv = Server(smoreoConfig, self.parametersCallback)
+        # self.srv = Server(smoreoConfig, self.parametersCallback)
         self.cutOffPublisher = self.create_publisher(Image,
-            self.get_parameter("/smoreo/cut_off_viz"), queue_size=10
+            self.get_parameter("/smoreo/cut_off_viz"), 10
         )
         self.bridge = CvBridge()
         self.lastImage = None
@@ -106,12 +115,12 @@ class TunerSystem(Node):
         # Get the parameters
         params = {
             "smoreo": {
-                "f": Tuner.node.get_parameter("smoreo/f"),
-                "cx": Tuner.node.get_parameter("smoreo/cx"),
-                "cy": Tuner.node.get_parameter("smoreo/cy"),
-                "cone_height": Tuner.node.get_parameter("smoreo/cone_height"),
-                "camera_height_from_ground": Tuner.node.get_parameter("smoreo/camera_height_from_ground"),
-                "cut_off_y": Tuner.node.get_parameter("smoreo/cut_off_y"),
+                "f": TunerSystem.get_parameter("smoreo/f").get_parameter_value().double_value,
+                "cx": TunerSystem.get_parameter("smoreo/cx").get_parameter_value().double_value,
+                "cy": TunerSystem.get_parameter("smoreo/cy").get_parameter_value().double_value,
+                "cone_height": TunerSystem.get_parameter("smoreo/cone_height").get_parameter_value().double_value,
+                "camera_height_from_ground": TunerSystem.get_parameter("smoreo/camera_height_from_ground").get_parameter_value().double_value,
+                "cut_off_y": TunerSystem.get_parameter("smoreo/cut_off_y").get_parameter_value().double_value,
             }
         }
         # Dump the parameters to a yaml file i the config folder
@@ -122,29 +131,31 @@ class TunerSystem(Node):
 
         with open(absFilePath, "w", encoding="utf-8") as file:
             yaml.dump(params, file)
-
-
+    def timer_callback(self):        
+        self.visualizeCutOff()
+        # self.tunerStatus.running()
 
 def main(args = None) -> None:
     """
     Main Loop
     """
-  
+
     rclpy.init(args = args)
     node = TunerSystem()
 
     rclpy.spin(node)
     rclpy.shutdown()
 
-
 if __name__ == "__main__":
+    print("Tuner Node")
     try:
         main()
     except rclpy.exceptions.ROSInterruptException:
         pass
+    
     finally:
         try:
             assert len(sys.argv) > 2
         except Exception as exc:
             raise ValueError("Provide the name of the setup you are tuning") from exc
-        Tuner.dumpParams(sys.argv[1])
+        TunerSystem.dumpParams(sys.argv[1])
