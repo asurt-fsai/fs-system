28cd98f08042b0399df5f47e7145ab1299c181c2
Mohamed Alaa <mohammed.alaa200080@gmail.com>
2024-04-19 16:31:57 +0200

    feat: adapt initializing project to ros2

--- initialize_project.sh
+++ initialize_project.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
+echo SCRIPT_DIR $SCRIPT_DIR
 PYTHON_SUFFIX=""
 if [ "$ROS_PYTHON_VERSION" = "3" ]; then
     PYTHON_SUFFIX=3
 fi
 
-ADDITIONAL_PACKAGES="ros-$ROS_DISTRO-rospy"
+ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES ros-$ROS_DISTRO-rcl"
+ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES ros-$ROS_DISTRO-rclpy"
+ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES ros-$ROS_DISTRO-rclcpp"
 ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES python3-pcl" #perception/mrpython-pcl
 ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES ros-$ROS_DISTRO-velodyne" #dependancies velodyne
-
 
 if [ "$(lsb_release -sc)" = "focal" ]; then
     ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES
@@ -18,13 +19,15 @@ if [ "$(lsb_release -sc)" = "focal" ]; then
 fi
 
 
+echo PYTHON SUFFIX $PYTHON_SUFFIX
+echo ros distro $ROS_DISTRO
 echo ADDITIONAL PACKAGES $ADDITIONAL_PACKAGES
 
 sudo apt update
 sudo apt-get install --no-install-recommends -y \
     python$PYTHON_SUFFIX-pip \
-    python$PYTHON_SUFFIX-catkin-tools \
     python$PYTHON_SUFFIX-rosdep \
+    python$PYTHON_SUFFIX-rosinstall \
     wget \
     $ADDITIONAL_PACKAGES
 
