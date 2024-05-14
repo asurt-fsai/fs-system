ff8625e8003ce4aa6bf845842989b9110bc9c13b
juyasser <101010604+juyasser@users.noreply.github.com>
2024-05-14 12:58:33 +0300

    add: unknown cones

--- planning/deep_learning/notebooks/noise.py
+++ planning/deep_learning/notebooks/noise.py
@@ -5,38 +5,6 @@ import os
 def add_noise(value, noise_factor=0.05):
     noise = random.uniform(-noise_factor, noise_factor)
     return value + noise
-def modify_cones(data, change_percentage=0.05):
-    modified_data = {'x': [], 'y': [], 'color': []}
-    blue_count = 0
-    yellow_count = 0
-    for x, y, color in zip(data['x'], data['y'], data['color']):
-        if color == 'yellow':
-            if yellow_count < len(data['color']) * change_percentage:
-                modified_data['x'].append(add_noise(x))
-                modified_data['y'].append(add_noise(y))
-                modified_data['color'].append('blue')
-                blue_count += 1
-            else:
-                modified_data['x'].append(add_noise(x))
-                modified_data['y'].append(add_noise(y))
-                modified_data['color'].append('yellow')
-            yellow_count += 1
-        elif color == 'blue':
-            if blue_count < len(data['color']) * change_percentage:
-                modified_data['x'].append(add_noise(x))
-                modified_data['y'].append(add_noise(y))
-                modified_data['color'].append('yellow')
-                yellow_count += 1
-            else:
-                modified_data['x'].append(add_noise(x))
-                modified_data['y'].append(add_noise(y))
-                modified_data['color'].append('blue')
-            blue_count += 1
-        else:  # orange cones remain unchanged
-            modified_data['x'].append(add_noise(x))
-            modified_data['y'].append(add_noise(y))
-            modified_data['color'].append('orange')
-    return modified_data
 
 
 def main():
@@ -47,7 +15,7 @@ def main():
     for filename in os.listdir(folder_path):
         if filename.endswith('.json'):
             input_file = os.path.join(folder_path, filename)
-            output_file = os.path.join(folder_path, 'modified_' + filename)
+            output_file = os.path.join(output_path, 'modified_' + filename)
             
             with open(input_file, 'r') as f:
                 cones_data = json.load(f)
@@ -56,13 +24,13 @@ def main():
                 'x': cones_data['x'],
                 'y': cones_data['y'],
                 'color': cones_data['color'],
-                'pathX': cones_data['pathX'],
-                'pathY': cones_data['pathY']
+                'x_path': cones_data['pathX'],
+                'y_path': cones_data['pathY']
             }
             for i, color in enumerate(cones_data['color']):
                 if color == 'blue' or color == 'yellow':
                     if random.random() < change_percentage:
-                        modified_data['color'][i] = 'yellow' if color == 'blue' else 'blue'
+                        modified_data['color'][i] = random.choice(['unknown', 'yellow']) if color == 'blue' else random.choice(['blue', 'unknown'])
                 elif color == 'orange_big':
                     modified_data['color'][i] = 'orange_big'
                 elif color == 'orange':
