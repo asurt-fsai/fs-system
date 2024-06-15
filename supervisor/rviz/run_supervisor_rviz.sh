684f33e112f252df00cfc1bf87cd751c25516768
Mohamed Alaa <mohammed.alaa200080@gmail.com>
2024-06-15 15:43:44 +0300

    build: remove supervisor folders

--- supervisor/rviz/run_supervisor_rviz.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /opt/ros/noetic/setup.bash
-source /home/nvidia/catkin_fs/devel/setup.bash
-bash -c "rosrun rviz rviz -d /home/nvidia/catkin_fs/src/supervisor/rviz/supervisor.rviz"
-
-exec $SHELL
