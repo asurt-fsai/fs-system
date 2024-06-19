0849766775bc546b47074f831a5e1ff2cd0d27d9
Mohamed-alaa32 <mohammed.alaa200080@gmail.com>
2024-06-19 09:05:21 +0300

    build: re-add smornn module

---
+++ perception/smornn/setup.py
@@ -0,0 +1,11 @@
+# pylint: disable=all
+# mypy: ignore-errors
+"""
+Install the smornn library
+"""
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(packages=["smornn"], package_dir={"": "src"})
+
+setup(**d)
