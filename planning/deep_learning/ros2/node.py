c3484dec2529bd1f5ce1be5ff4ccee232f3ea018
Jumana <jumana.yasser777@gmail.com>
2024-06-21 18:30:31 -0400

    fix: clean node.py

--- planning/deep_learning/ros2/node.py
+++ planning/deep_learning/ros2/node.py
@@ -1,109 +1,152 @@
 #!/usr/bin/env python3
-
+"""Importing necessary libraries for the ros2 node"""
+from typing import Any
+import sys
+from tf_helper import TFHelper
 import rclpy
 from rclpy.node import Node
-from asurt_msgs.msg import Landmark
+import torch
 from asurt_msgs.msg import LandmarkArray
-from nav_msgs.msg import Odometry, Path
+from nav_msgs.msg import Path
 from geometry_msgs.msg import Pose, PoseStamped
-from model import MyPlannerTransformer
-from model import PathPredict
-from model import createModel
+from planning_deep_learning import model
 import numpy as np
 
+sys.path.append("/home/ubuntu/ros2_ws/src/tf_helper/")
 
-class PlanningDlNode(Node):
 
-    def __init__(
-        self,
-        modelPath: str
-        ):
+class PlanningDlNode(Node):  # type: ignore
+    """
+    ROS2 node for deep learning-based path planning.
+
+    This node receives landmarks from perception, processes them, and uses a deep learning model
+    to predict a path based on the processed landmarks. If a valid path is obtained, it publishes
+    the path as a ROS2 Path message.
+
+    Attributes:
+        model: The deep learning model used for path prediction.
+        path: The predicted path.
+        conesList: The processed list of cones.
+        tfhelper: The TFHelper object for transforming messages.
+        subscriber1: The subscriber for receiving landmarks from perception.
+        publisher: The publisher for publishing the predicted path.
+
+    Methods:
+        __init__: Initializes the PlanningDlNode object.
+        receiveFromPerception: Receives a message from perception and processes the landmarks.
+        sendToControl: Sends the predicted path to the control system.
+    """
+
+    def __init__(self) -> None:
         super().__init__("planning_dl")
         self.get_logger().info("DL Path Planner instantiated...")
-        self.model = createModel(modelPath)
+        modelPath = (
+            "/home/ubuntu/ros2_ws/src/planning_deep_learning/"
+            "planning_deep_learning/model_after_Last_Train.pt"
+        )
+        self.model = model.createModel(modelPath)
         self.path = None
-        self.cones = None
-        #self.carPosition = None
+        self.conesList = Any
+        self.tfhelper = TFHelper.TFHelper(self)
+        # self.carPosition = None
         self.subscriber1 = self.create_subscription(
-            LandmarkArray, "/topic1", self.receive_from_perception, 10
+            LandmarkArray, "/Landmarks/Observed", self.receive_from_perception, 10
         )
-        self.publisher = self.create_publisher(
-            Path, "/topic2", 10
-        )
+        self.publisher = self.create_publisher(Path, "/dl_path", 10)
 
-
-    def receive_from_perception(self, msg: LandmarkArray):
-        #get cones_colors, cones_positions
-        #self.cones = [np.zeros((0, 2)) for _ in ConeTypes]
+    def receiveFromPerception(self, msg: LandmarkArray) -> None:
         """
-        for landmark in msg.landmarks:
-            if landmark.identifier == 0:
-                self.cones[ConeTypes.BLUE] = np.vstack((self.cones[ConeTypes.BLUE], landmark.position))
-            elif landmark.identifier == 1:
-                self.cones[ConeTypes.YELLOW] = np.vstack((self.cones[ConeTypes.YELLOW], landmark.position))
-            elif landmark.identifier == 3:
-                self.cones[ConeTypes.ORANGE] = np.vstack((self.cones[ConeTypes.ORANGE], landmark.position))
-            elif landmark.identifier == 4:
-                self.cones[ConeTypes.UNKNOWN] = np.vstack((self.cones[ConeTypes.UNKNOWN], landmark.position))
-           """     
-        
-        self.cones = []
+        Receives a message from perception and processes the landmarks.
+
+        Args:
+            msg (LandmarkArray): The message containing the landmarks.
+
+        Returns:
+            None
+        """
+
+        self.get_logger().info("Message received...")
+
+        msg = self.tfhelper.transformMsg(msg, "Obj_F")
+        self.conesList = []
 
         # Process incoming landmarks
         for landmark in msg.landmarks:
+            cones = []
             # Encode color one-hot
-            #color_one_hot = [0, 0, 0, 0]  # [blue, yellow, orange, unknown]
-            color_one_hot = [0, 0, 0]  # [blue, yellow, orange]
+            colorOneHot = [0, 0, 0, 0]  # [blue, yellow, orange, unknown]
+            # color_one_hot = [0, 0, 0]  # [blue, yellow, orange]
             if landmark.identifier == 0:
-                color_one_hot[0] = 1
+                colorOneHot[0] = 1
+                cones.append(np.array([landmark.position.x, landmark.position.y] + colorOneHot))
             elif landmark.identifier == 1:
-                color_one_hot[1] = 1
+                colorOneHot[1] = 1
+                cones.append(np.array([landmark.position.x, landmark.position.y] + colorOneHot))
             elif landmark.identifier == 3:
-                color_one_hot[2] = 1
-            #else:
-                #color_one_hot[3] = 1
-            cone_data = np.concatenate((landmark.position, color_one_hot))
-            self.cones.append(cone_data)
+                colorOneHot[2] = 1
+                cones.append(np.array([landmark.position.x, landmark.position.y] + colorOneHot))
+            else:
+                colorOneHot[3] = 1
+                cones.append(np.array([landmark.position.x, landmark.position.y] + colorOneHot))
 
+            # Vertically stack the arrays
+            self.conesList = np.vstack(cones)
         # Convert to torch tensor
-        self.cones = torch.tensor(self.cones, dtype=torch.float32)
-            
-        self.send_to_control()
+        # Convert list of lists to single NumPy array
+        conesArray = np.array(self.conesList, dtype=np.float32)  # Assuming all elements are numeric
+        # Convert NumPy array to tensor
+        self.conesList = torch.tensor(conesArray)
+        self.sendToControl()
 
-    
-    def send_to_control(self):
-        self.path = self.model.pathPredict(self.cones)
+    def sendToControl(self) -> None:
+        """
+        Sends the predicted path to the control system.
+
+        This method uses the model to predict the path based on the cones list.
+        If a valid path is obtained, it creates a ROS2 Path message and publishes it.
+        The path is constructed by converting each data point in the predicted path
+        to a PoseStamped message and appending it to the Path message.
+
+        Returns:
+            None
+        """
+        self.path = self.model.predict(self.conesList)
 
         if self.path is not None:
-            timestamp = rclpy.time.now()
+            timestamp = self.get_clock().now().to_msg()
             path = Path()
             path.header.stamp = timestamp
             path.header.frame_id = "dl_path"
-            
+
             for dataPoint in self.path:
                 pose = Pose()
-                pose.position.x = dataPoint[0]
-                pose.position.y = dataPoint[1]
+                pose.position.x = float(dataPoint[0])
+                pose.position.y = float(dataPoint[1])
 
                 poseStamped = PoseStamped()
                 poseStamped.pose = pose
                 poseStamped.header.stamp = timestamp
-                poseStamped.header.frame_id = "dl_path"
+                poseStamped.header.frame_id = "map"
 
                 path.poses.append(poseStamped)
 
             self.publisher.publish(path)
 
-            self.get_logger().info('DL Path Sent...')
-            
+            self.get_logger().info("DL Path Sent...")
 
 
+def main(args: None = None) -> None:
+    """
+    Entry point of the program.
 
-def main(args=None):
+    Args:
+        args: Command-line arguments (default: None).
+    """
     rclpy.init(args=args)
     node = PlanningDlNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
