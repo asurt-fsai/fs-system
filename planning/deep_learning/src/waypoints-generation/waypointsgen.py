7fa451dde941023d40173991a813de87665759a2
Jumana <jumana.yasser777@gmail.com>
2024-06-29 16:49:14 -0400

    fix: cleaning waypointsgen.py

--- planning/deep_learning/src/waypoints-generation/waypointsgen.py
+++ planning/deep_learning/src/waypoints-generation/waypointsgen.py
@@ -1,45 +1,45 @@
-from Datagen import *
-
-import time
+'''importing the necessary libraries for generating waypoints'''
 import os
+import torch
+from matplotlib import plt
+from Trackgen import TrackGenerator
+from Datagen import DataGenerator
 
-count = 0
+
+COUNT = 0
 for j in range(750):
-    
+
     trackGen = TrackGenerator()
-    track = trackGen.generate_track(20)
-    smoothed = trackGen.smooth_track(*track, 5)
-    spline = trackGen.fit_spline(*smoothed, 300)
-    cones_data = trackGen.generate_cones(*spline, track_width=2, distance_between_cones=10)
+    track = trackGen.generateTrack(20)
+    smoothed = trackGen.smoothTrack(track, 5)
+    spline = trackGen.fitSpline(smoothed, 300)
+    conesData = trackGen.generateCones(spline[0], spline[1], trackWidth=2, distanceBetweenCones=10)
+    dataGen = DataGenerator(conesData, spline)
 
-    dataGen = DataGenerator(cones_data, spline)
-
-    src,tgt = dataGen.generate_data()
+    src,tgt = dataGen.generateData()
     src = dataGen.oneHotEncode(src)
-    src,tgt = dataGen.ToTensors(src,tgt)
+    src,tgt = dataGen.toTensors(src,tgt)
     plt.figure()
-    trackGen.plot_track(spline[0], spline[1])
-    
+    trackGen.plotTrack(spline[0], spline[1])
     src_file_path = f"inputs/tensorsV2/inputs/train/src{j}.pt"
     tgt_file_path = f"inputs/tensorsV2/outputs/train/tgt{j}.pt"
-    
-    for i in cones_data:
-        trackGen.plot_cones(cones_data[i][0], cones_data[i][1], color=i)
 
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.axis
+    for i, cones in conesData.items():
+        trackGen.plotCones(cones[0], cones[1], color=i)
+
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.show(block = False)
     plt.pause(1)
     plt.close()
-    
+
     while os.path.exists(src_file_path) or os.path.exists(tgt_file_path):
         j += 1
         src_file_path = f"inputs/tensorsV2/inputs/train/src{j}.pt"
         tgt_file_path = f"inputs/tensorsV2/outputs/train/tgt{j}.pt"
-    
+
     torch.save(src, src_file_path)
     torch.save(tgt, tgt_file_path)
-    print("count: ", count+1)
-    count += 1
+    print("count: ", COUNT+1)
+    COUNT += 1
 print("Data saved to src.pt and tgt.pt")
